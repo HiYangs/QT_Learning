3c102b7fcb66177ce86dfee976b732c8e8c1b51d
sean.yang <sean.yang@hzleaper.com>
2016-04-24 21:40:23 +0800

    4/24

--- QTGUI/QTChapter2/finddialog.cpp
+++ QTGUI/QTChapter2/finddialog.cpp
@@ -1,11 +1,36 @@
 #include "finddialog.h"
+#include <QtGui>
+#include <QLabel>
+#include <QlineEdit>
+#include <QCheckBox>
+#include <QPushButton>
 
-
-finddialog::finddialog()
+FindDialog::FindDialog(QWidget *parent)
+	: QDialog(parent) //! 把parent参数传递给了基类的构造函数
 {
+	//! 在这些字符串中，使用了“&”来表示快捷键
+	//! 符号“&”可以用来控制焦点
+	label = new QLabel(tr("Find &what:"));
+	lineEdit = new QLineEdit;
+	label->setBuddy(lineEdit);
+
+	caseCheckBox = new QCheckBox("Match &case");
+	backwardCheckBox = new QCheckBox(tr("Search &backward"));
+
+	findButton = new QPushButton(tr("&Find"));
+	findButton->setDefault(true);
+	findButton->setEnabled(false);
+
+	closeButton = new QPushButton(tr("Close"));
 }
 
-
-finddialog::~finddialog()
+void FindDialog::enableFindButton(const QString &text)
 {
+
 }
+
+void FindDialog::findClicked()
+{
+
+}
+
--- QTGUI/QTChapter2/finddialog.h
+++ QTGUI/QTChapter2/finddialog.h
@@ -16,6 +16,22 @@ class FindDialog : public QDialog
 public:
 	FindDialog(QWidget *parent = 0);
 
+	//!
+signals:
+	void findNext(const QString &str, Qt::CaseSensitivity cs);
+	void findPrevious(const QString &str, Qt::CaseSensitivity cs);
+
+private slots:
+	void findClicked();
+	void enableFindButton(const QString &text);
+
+private:
+	QLabel *label;
+	QLineEdit *lineEdit;
+	QCheckBox *caseCheckBox;
+	QCheckBox *backwardCheckBox;
+	QPushButton *findButton;
+	QPushButton *closeButton;
 };
 #endif
 
